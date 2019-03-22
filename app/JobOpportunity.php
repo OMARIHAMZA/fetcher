cbc680308f5c2931c9d6ad4e45e73be2f28b34f7
Samer Alsaydali <samsaydali@gmail.com>
2019-03-22 19:29:18 +0200

    Fix Job Opportunity Query

--- app/JobOpportunity.php
+++ app/JobOpportunity.php
@@ -11,11 +11,19 @@ class JobOpportunity extends Opportunity
 
     public function getByCategory(Category $category)
     {
-        return $category->jobOpportunities()->join('companies', 'company_id', '=', 'companies.id')->get();
+        return $category->jobOpportunities()->join('companies', 'company_id', '=', 'companies.id')->get(
+            ['job_opportunities.id as job_opportunity_id','companies.id as company_id','duration','companies.name as company_name','duration',
+                'end_of_submission','start','requirements','number_of_employees','salary',
+                'place','days','title']
+        );
     }
 
     public function getByCompany(Company $company)
     {
-        return $company->jobOpportunities()->join('categories', 'category_id', '=', 'categories.id')->get();
+        return $company->jobOpportunities()->join('categories', 'category_id', '=', 'categories.id')->get(
+            ['job_opportunities.id as job_opportunity_id','categories.id as category_id','categories.name as category_name','duration',
+                'end_of_submission','start','requirements','number_of_employees','salary',
+                'place','days','title']
+        );
     }
 }
