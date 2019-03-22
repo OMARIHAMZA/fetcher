46e25de4e5ea570482fe45fe0366cb9b0975946f
Samer Alsaydali <samsaydali@gmail.com>
2019-03-22 19:46:49 +0200

    Fix Training Opportunity Query

--- app/TrainingOpportunity.php
+++ app/TrainingOpportunity.php
@@ -10,11 +10,17 @@ class TrainingOpportunity extends Opportunity
 
     public function getByCategory(Category $category)
     {
-        return $category->trainingOpportunities()->join('companies', 'company_id', '=', 'companies.id')->get();
+        return $category->trainingOpportunities()->join('companies', 'company_id', '=', 'companies.id')->get(
+            array_merge(['training_opportunities.id as training_opportunity_id',
+                'companies.id as company_id','companies.name as company_name'], $this->fillable)
+    );
     }
 
     public function getByCompany(Company $company)
     {
-        return $company->trainingOpportunities()->join('categories', 'category_id', '=', 'categories.id')->get();
+        return $company->trainingOpportunities()->join('categories', 'category_id', '=', 'categories.id')->get(
+            array_merge(['training_opportunities.id as training_opportunity_id',
+                'categories.id as category_id','categories.name as category_name'], $this->fillable)
+        );
     }
 }
